783426cb7d70706684daccee6ee1eb4b0e0a886b
Dacian Rusu <rusudacian95@gmail.com>
2023-05-09 14:41:10 +0300

    welcome section

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
-import "./App.css";
+import { WelcomeSection } from "./components";
 
 function App() {
   return (
     <div className="w-screen h-screen relative">
       <div className="bg-[#00A884] h-[127px] w-full"></div>
       <div className="bg-[#E2E1DE] h-[calc(100vh-127px)]"></div>
-      <div className="absolute top-4 bottom-4 left-1/2 transform -translate-x-1/2 max-w-[1500px] w-full">
-        Here content is
+      <div className="absolute top-4 bottom-4 left-1/2 transform -translate-x-1/2 max-w-[1500px] w-full grid grid-cols-12">
+        <div className="col-span-4">Left side</div>
+        <div className="col-span-8">
+          <WelcomeSection />
+        </div>
       </div>
     </div>
   );
