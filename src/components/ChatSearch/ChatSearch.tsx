20c8ad5fa57b152213e3f04a26fb055dec457c46
Dacian Rusu <rusudacian95@gmail.com>
2023-05-09 15:10:03 +0300

    Chat search colors update

--- src/components/ChatSearch/ChatSearch.tsx
+++ src/components/ChatSearch/ChatSearch.tsx
@@ -6,11 +6,11 @@ interface ChatSearchProps {
 
 export function ChatSearch(props: ChatSearchProps) {
   return (
-    <div className="flex items-center gap-2 px-4">
+    <div className="flex items-center gap-2 px-4 py-2 bg-white">
       <div className="w-full relative h-[36px]">
         <Search className="absolute top-1/2 -translate-y-1/2 left-4" />
         <input
-          className="w-full h-full pl-14 pr-4 rounded-lg focus:outline-none"
+          className="w-full h-full pl-14 pr-4 rounded-lg focus:outline-none bg-[#F0F2F5]"
           type="text"
           placeholder="Search or start new chat"
           onChange={(event) => props.onSearch(event.target.value)}
