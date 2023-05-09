6f44dc4f8d92bf1327082565b59f129744065392
Dacian Rusu <rusudacian95@gmail.com>
2023-05-09 14:29:44 +0300

    setup app layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
-import React from 'react';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
-    <div className="flex items-center justify-between">
-      Item 1
-      Item 2
+    <div className="w-screen h-screen relative">
+      <div className="bg-[#00A884] h-[127px] w-full"></div>
+      <div className="bg-[#E2E1DE] h-[calc(100vh-127px)]"></div>
+      <div className="absolute top-4 bottom-4 left-1/2 transform -translate-x-1/2 max-w-[1500px] w-full">
+        Here content is
+      </div>
     </div>
   );
 }
