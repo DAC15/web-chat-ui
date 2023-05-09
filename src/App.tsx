2d378477b07f913f407c3ccf1786f181fe40db7b
Dacian Rusu <rusudacian95@gmail.com>
2023-05-09 14:20:31 +0300

    init tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,11 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React here
-        </a>
-      </header>
+    <div className="flex items-center justify-between">
+      Item 1
+      Item 2
     </div>
   );
 }
